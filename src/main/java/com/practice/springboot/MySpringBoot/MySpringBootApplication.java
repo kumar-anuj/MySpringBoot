bb7e9f050e0fadbf18a52886c6ecebdba92af0f6
Anuj Kumar <87.anuj.kumar@gmail.com>
2019-03-21 21:30:18 +0530

    spring boot setup

--- src/main/java/com/practice/springboot/MySpringBoot/MySpringBootApplication.java
+++ src/main/java/com/practice/springboot/MySpringBoot/MySpringBootApplication.java
@@ -2,12 +2,27 @@ package com.practice.springboot.MySpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class MySpringBootApplication {
+@RestController
+public class MySpringBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringBootApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MySpringBootApplication.class);
+	}
+
+	@RequestMapping(path="/hello")
+	public String helloWorld() {
+		return "Hello World !";
 	}
 
 }
